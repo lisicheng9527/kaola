eaa24fd1251d371c4bee0b4ef1f0d656f992910e
lisicheng <lisicheng9527@163.com>
2017-12-07 18:20:49 +0800

    first commit

---
+++ src/component/home/home.js
@@ -0,0 +1,13 @@
+import React,{ Component } from 'react';
+import {Button} from 'antd-mobile';
+import './css/home.less';
+
+class Home extends Component {
+    render(){
+        return(
+            <div className="test"><h1>test!</h1></div>
+        )
+    }
+}
+
+export default Home;
---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import App from './App';
+import Home from './component/home/home';
+import registerServiceWorker from './registerServiceWorker';
+
+ReactDOM.render((
+    <Router>
+        <Route exact path="/" component={Home}/>
+    </Router>
+  ), document.getElementById('root'));
+registerServiceWorker();
